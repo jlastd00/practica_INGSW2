80c33cbd9bf56cbc3955bfb4d6aed789b263d572
jlastd00 <jlastd00@estudiantes.unileon.es>
2018-04-25 20:45:34 +0200

    añadidas clases entidad

--- peaje/src/main/java/INSO2/peaje/modelo/Usuario.java
+++ peaje/src/main/java/INSO2/peaje/modelo/Usuario.java
@@ -5,10 +5,112 @@
  */
 package INSO2.peaje.modelo;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
- *
  * @author juanra
  */
-public class Usuario {
+
+@Entity
+@Table(name="usuarios")
+
+public class Usuario implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int idUsuario;
+    
+    @Column(name="dni")
+    private String dni;
+    
+    @Column(name="nombre")
+    private String nombre;
+    
+    @Column(name="apellido1")
+    private String apellido1;
+    
+    @Column(name="apellido2")
+    private String apellid02;
+    
+    @Column(name="usuario")
+    private String usuario;
+    
+    @Column(name="password")
+    private String password;
+    
+    @Column(name="rol")
+    private String rol;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellid02() {
+        return apellid02;
+    }
+
+    public void setApellid02(String apellid02) {
+        this.apellid02 = apellid02;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    
+    
     
 }
--- peaje/src/main/java/INSO2/peaje/modelo/Vehiculo.java
+++ peaje/src/main/java/INSO2/peaje/modelo/Vehiculo.java
@@ -5,10 +5,120 @@
  */
 package INSO2.peaje.modelo;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 /**
  *
  * @author juanra
  */
-public class Vehiculo {
+
+@Entity
+@Table(name="categorias")
+
+public class Vehiculo implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int idVehiculo;
+    
+    @Column(name="matricula")
+    private String matricula;
+    
+    @Column(name="tipoVehiculo")
+    private String tipoVehiculo;
+    
+    @Column(name="marca")
+    private String marca;
+    
+    @Column(name="modelo")
+    private String modelo;
+    
+    @Column(name="color")
+    private String color;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="fechaRegistro")
+    private Date fechaRegistro;
+    
+    @ManyToOne
+    @JoinColumn(name="idUsuario", nullable=false)
+    private Usuario usuario;
+
+    public int getIdVehiculo() {
+        return idVehiculo;
+    }
+
+    public void setIdVehiculo(int idVehiculo) {
+        this.idVehiculo = idVehiculo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getTipoVehiculo() {
+        return tipoVehiculo;
+    }
+
+    public void setTipoVehiculo(String tipoVehiculo) {
+        this.tipoVehiculo = tipoVehiculo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    
     
 }
